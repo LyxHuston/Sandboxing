e2118a6ac13e36f139d93f5e0100f7bca1a7b0d1
Lyx Huston <lyx@hustonfam.net>
2023-09-28 17:05:56 -0400

    changed the sandbox call to more closely match importlib's import_module function.  It previously matched the syntax for the builtin import function, which didn't make much sense.

--- sandbox_import.py
+++ sandbox_import.py
@@ -33,8 +33,7 @@ import marshal
 
 import frozendict
 import time
-from importlib._bootstrap import _builtin_from_name
-_imp = _builtin_from_name("_imp")
+import _imp
 
 
 try:
@@ -204,41 +203,25 @@ class RestrictedImport:
         cop["__spec__"].loader = None
         return cop
 
-    def __call__(self, name, globals=None, locals=None, fromlist=(), level=0):  # TODO potential safety concern in globals or locals
-        """Import a module.
-
-        The 'globals' argument is used to infer where the import is occurring from
-        to handle relative imports. The 'locals' argument is ignored. The
-        'fromlist' argument specifies what should exist as attributes on the module
-        being imported (e.g. ``from module import <fromlist>``).  The 'level'
-        argument represents the package location to import from in a relative
-        import (e.g. ``from ..pkg import mod`` would have a 'level' of 2).
-
+    def __call__(self, name: str, package: str = None, specific_import_list: list[str] = ()):  # TODO potential safety concern in globals or locals
         """
-        if level == 0:
-            mod = self._gcd_import(name)
-        else:
-            globals_ = globals if globals is not None else {}
-            package = _calc___package__(globals_)
-            mod = self._gcd_import(name, package, level)
-        if not fromlist:
-            # Return up to the first dot in 'name'. This is complicated by the fact
-            # that 'name' may be relative.
-            if level == 0:
-                return self._gcd_import(name.partition('.')[0])
-            elif not name:
-                return mod
-            else:
-                # Figure out where to slice the module's name up to the first dot
-                # in 'name'.
-                cut_off = len(name) - len(name.partition('.')[0])
-                # Slice end needs to be positive to alleviate need to special-case
-                # when ``'.' not in name``.
-                return self._sys.modules[mod.__name__[:len(mod.__name__)-cut_off]]
-        elif hasattr(mod, '__path__'):
-            return self._handle_fromlist(mod, fromlist, self._gcd_import)
-        else:
+        import a module based on importlib's import_module function.
+        """
+        level = 0
+        if name.startswith('.'):
+            if not package:
+                msg = ("the 'package' argument is required to perform a relative "
+                       "import for {!r}")
+                raise TypeError(msg.format(name))
+            for character in name:
+                if character != '.':
+                    break
+                level += 1
+        mod = self._gcd_import(name[level:], package, level)
+        if len(specific_import_list) == 0:
             return mod
+        else:
+            return (getattr(mod, arg) for arg in specific_import_list)
 
     def make_import(self):
 
